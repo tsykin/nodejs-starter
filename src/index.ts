0a67d3b04aa4a0f36b860c95179080b983701100
tsykin <tsikinsasha@gmail.com>
2025-11-21 15:42:03 +0100

    add example script

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,10 @@
 // import * as path from 'path';
 // import { join } from 'path';
 // import { promisify } from 'util';
-import { CLIENT_ENV } from '@/shared/env.client';
 import { formatDate, generateRandomNumber } from '@/shared/utils';
 
 const now = new Date();
 const formattedDate = formatDate(now, 'DD Month YYYY');
 const randomN = generateRandomNumber(1, 100);
-console.log(`Environment: ${CLIENT_ENV.VITE_BASE_URL}`);
 console.log(`Current timestamp: ${formattedDate}`);
 console.log(`Random number: ${randomN}`);
